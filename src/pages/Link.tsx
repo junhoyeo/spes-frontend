ae40f8f7162a9d92cfaae45b8153673c129df37f
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2020-02-05 22:33:38 +0900

    feat: redesign link page

--- src/pages/Link.tsx
+++ src/pages/Link.tsx
@@ -1,44 +1,68 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { NeuButton, NeuCard, NeuInput } from 'neumorphic-ui';
-import BrandCard from '../components/organisms/BrandCard';
-// import { FooterForLink } from '../components/organisms/Footer';
 
+import BrandCard from '../components/organisms/BrandCard';
 import { Page } from '../components/atoms/Page';
-import Navbar from '../components/molecules/Navbar';
+import Text from '../components/atoms/Text';
+import Footer from '../components/organisms/Footer';
+import Input from '../components/molecules/Input';
+import Button from '../components/atoms/Button';
 
 const Link: React.FC = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
   return (
     <Page>
-      {/* <Navbar
-        title="목표방 입장하기"
-        hideMeta={true}
-      />
-      <NeuCard>
-        <CardContent>
-          <Section>
-            <BrandCard
-              title="친구가 Spes™를 함께 사용하고 싶다네요."
-              src="https://avatars0.githubusercontent.com/u/32605822?s=200"
-            />
-          </Section>
-          <Form>
-            <FormTitle>
-              이미 있는 계정으로 로그인을 해주세요.
-            </FormTitle>
-            <NeuInput
-              placeholder="이메일"
-            />
-            <NeuInput
-              placeholder="비밀번호"
-            />
-            <NeuButton text="로그인" />
-            <NeuButton text="계정 생성하러 가기" />
-          </Form>
-        </CardContent>
-      </NeuCard> */}
+      <Header>
+        <BrandCard
+          title={
+            <Title>
+              여러분의 친구가 함께<br />
+              Spes™로 목표를 이루고 싶다네요.<br />
+              로그인해서 함께할 수 있어요.
+            </Title>
+          }
+          src="https://avatars0.githubusercontent.com/u/32605822?s=200"
+        />
+      </Header>
+      <Form>
+        <Input
+          label="이메일"
+          type="email"
+          value={email}
+          placeholder="이메일을 입력해 주세요."
+          onChange={(e: any) => setEmail(e.target.value)}
+        />
+        <Input
+          label="비밀번호"
+          type="password"
+          value={password}
+          placeholder="비밀번호를 안전하게 입력하세요."
+          onChange={(e: any) => setPassword(e.target.value)}
+        />
+        <Button>
+          로그인으로 초대장 수락하기
+        </Button>
+      </Form>
+      <Footer />
     </Page>
   );
 };
 
 export default Link;
+
+const Header = styled.header`
+  margin-bottom: 2.5rem;
+`;
+
+const Form = styled.form`
+  width: 100%;
+  margin-bottom: 2rem;
+`;
+
+const Title = styled(Text)`
+  font-size: 15px;
+  line-height: 1.5;
+  text-align: center;
+`;
